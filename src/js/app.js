99ab4bf57652a199f7b760889d7d332896649dda
MarcosJVPR <perezmarcosjulio@gmail.com>
2025-05-19 17:55:27 +0000

    changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,16 @@
 import { name } from "file-loader";
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  console.log("These are the current variables: ", variables);
+  if (variables.name == null) {
+    variables.name = "N/A";
+  }
+  if (variables.lastName == null) variables.lastName = "N/A";
+  if (variables.role == null) variables.role = "N/A";
+  if (variables.city == null) variables.city = "N/A";
+  if (variables.country == null) variables.country = "NA";
+
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
@@ -67,11 +51,11 @@ window.onload = function() {
     github: null,
     linkedin: null,
     instagram: null,
-    name: "Name, ",
-    lastName: "Last Name",
-    role: "Role",
-    country: "Country",
-    city: "City"
+    name: null,
+    lastName: null,
+    role: null,
+    country: null,
+    city: null
   };
   render(window.variables); // render the card for the first time
 
